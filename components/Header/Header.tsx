9f088a549afd071c5dd1c1af7e3b8a781ee28b22
Andrey Kudryavtsev <lomastering@gmail.com>
2021-03-30 18:43:37 +0300

    feat: header effect

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,23 +1,42 @@
 import Link from 'next/link';
-import React, { FC } from 'react';
+import React, { FC, useRef, useEffect } from 'react';
 
 import ContentContainer from '../ContentContainer/ContentContainer';
 import MainMenu from '../MainMenu/MainMenu';
 import styles from './Header.module.scss';
 
-const Header: FC = () => (
-  <div className={styles.header}>
-    <ContentContainer>
-      <div className={styles.header__content}>
-        <Link href="/">
-          <a href="/" className={styles.header__logo} tabIndex={-1}>
-            НПП Реставрационный центр
-          </a>
-        </Link>
-        <MainMenu />
-      </div>
-    </ContentContainer>
-  </div>
-);
+const Header: FC = () => {
+  const header = useRef<HTMLHeadingElement>(null);
+
+  const fillHeader = () => {
+    if (header.current) {
+      if (window.scrollY > 56) {
+        header.current.classList.add(styles.header_dark);
+      } else if (window.scrollY < 56) {
+        header.current.classList.remove(styles.header_dark);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', fillHeader);
+    return () => window.removeEventListener('scroll', fillHeader);
+  }, [fillHeader]);
+
+  return (
+    <header className={styles.header} ref={header}>
+      <ContentContainer>
+        <div className={styles.header__content}>
+          <Link href="/">
+            <a href="/" className={styles.header__logo} tabIndex={-1}>
+              НПП Реставрационный центр
+            </a>
+          </Link>
+          <MainMenu />
+        </div>
+      </ContentContainer>
+    </header>
+  );
+};
 
 export default Header;

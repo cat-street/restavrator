774e56977567907d15296716ce190c123a46ba1f
Andrey Kudryavtsev <lomastering@gmail.com>
2021-06-09 13:51:08 +0300

    fix: featured links

--- pages/books/[id]/index.tsx
+++ pages/books/[id]/index.tsx
@@ -1,10 +1,20 @@
 import { useRouter } from 'next/router';
 
+import BookPage from 'components/BookPage/BookPage';
+import Layout from 'components/Layout/Layout';
+
+import books from 'data/books.json';
+
 const index = (): JSX.Element => {
   const router = useRouter();
-  const { id } = router.query;
+  const id = router.query.id as string | undefined;
+  const currentBook = books.items.find((el) => el.id === id);
 
-  return <p>{id}</p>;
+  return (
+    <Layout title={`${currentBook?.title} - НПП Реставрационный центр`}>
+      {currentBook && <BookPage book={currentBook} />}
+    </Layout>
+  );
 };
 
 export default index;

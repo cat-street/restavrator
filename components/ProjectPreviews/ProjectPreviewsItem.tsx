579d5bf3673042783c19a26a4d5eace57edd785e
Andrey Kudryavtsev <lomastering@gmail.com>
2021-06-25 08:58:04 +0300

    fix: links

--- components/ProjectPreviews/ProjectPreviewsItem.tsx
+++ components/ProjectPreviews/ProjectPreviewsItem.tsx
@@ -25,11 +25,23 @@ const ProjectPreviewsItem = ({
         }`}
         className={styles['previews__image-container']}
       >
-        <img src={project.cover} alt={project.title} className={styles.previews__image} />
+        <img
+          src={project.cover}
+          alt={project.title}
+          className={styles.previews__image}
+        />
       </a>
     </Link>
-    <Link href={`/${category}/${project.id}`}>
-      <a href={`/${category}/${project.id}`} className={styles.previews__title}>
+
+    <Link
+      href={`/${category}/${subCategory ? `${subCategory}/` : ''}${project.id}`}
+    >
+      <a
+        href={`/${category}/${subCategory ? `${subCategory}/` : ''}${
+          project.id
+        }`}
+        className={styles.previews__title}
+      >
         {project.title}
       </a>
     </Link>
